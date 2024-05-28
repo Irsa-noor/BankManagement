ff2955299da5e5036bfe4941abbd85f418499d5c
Irsa-noor <irsanoor30@gmail.com>
2024-05-28 08:57:31 -0700

    Update BankManagement.java

--- BankManagement.java
+++ BankManagement.java
@@ -1,10 +1,10 @@
-package bankmanagement;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
- import javax.swing.*;
+package bankmanagement;
+import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
